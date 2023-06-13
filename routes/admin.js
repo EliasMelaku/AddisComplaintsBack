cf1e35e8f612f7061d126889ed5716d446da3ad7
Eyob Alemu <it.eyob.alemu@gamil.com>
2023-06-13 15:43:10 +0300

    add routes to access users and feedback for admin

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const crypto = require("crypto");
 const db = require("../models");
-const { DataTypes } = require("sequelize");
-const User = require("../models/user")(db.sequelize, DataTypes);
 
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
@@ -12,20 +10,48 @@ const router = express.Router();
 
 router.get("/getAllUsers", authenticate("admin"), async (req, res, next) => {
   try {
-    const users = await User.findAll();
+    const users = await db.User.findAll({
+      attributes: ["id", "name", "email", "role", "isBanned"],
+    });
     res.status(200).json(users);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
 });
 
-router.post(
-  "/getAllFeedback",
-  authenticate("admin"),
-  async (req, res, next) => {}
-);
+router.get("/getAllFeedback", authenticate("admin"), async (req, res, next) => {
+  try {
+    const feedbacks = await db.Feedback.findAll({
+      attributes: ["id", "name", "email", "comment", "pdf"],
+    });
 
-router.post("/banUser", authenticate("admin"), async (req, res, next) => {});
+    res.status(200).json(feedbacks);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.put(
+  "/banUser/:userId",
+  authenticate("admin"),
+  async (req, res, next) => {
+    try {
+      const user = await db.User.findOne({
+        where: { id: req.params.userId },
+      });
+      if (user) {
+        user.update({ isBanned: true });
+        await user.save();
+        console.log(user);
+        res.status(200).json({ message: "User banned!" });
+      } else {
+        res.status(404).json({ message: "User not found!" });
+      }
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  }
+);
 router.post(
   "/deleteComment",
   authenticate("admin"),
