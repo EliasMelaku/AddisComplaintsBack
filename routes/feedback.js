64a54eccae45505d992c7f64e7ca9e80cfd94c4b
Elias Melaku <se.elias.melaku@gmail.com>
2023-06-14 10:37:53 +0300

    more changes

--- routes/feedback.js
+++ routes/feedback.js
@@ -9,7 +9,22 @@ require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const { authenticate } = require("../middleware/authenticate");
 
+const csrf = require("@dr.pogodin/csurf");
+
+const csrfProtection = csrf({ cookie: true });
+
 const router = express.Router();
+
+router.get("/csrf-token", csrfProtection, (req, res, next) => {
+  // res.clearCookie("_csrf")
+  // res.send({ csrfToken: req.csrfToken });
+  res.cookie("XSRF-TOKEN", req.csrfToken()).status(200).send("Csrf-protected");
+});
+
+router.post("/clear", csrfProtection, (req, res, next) => {
+  // res.send("You passed the csrfCheck");
+  res.clearCookie("XSRF-TOKEN").status(200).send("");
+});
 
 router.post("/single", authenticate("user"), async (req, res, next) => {
   // console.log(req.body.id);
@@ -17,7 +32,9 @@ router.post("/single", authenticate("user"), async (req, res, next) => {
     const feedback = await db.Feedback.findOne({
       where: { id: req.body.id },
     });
-    feedback.pdf = feedback.pdf.split("~~")[0];
+    if (feedback.pdf !== null) {
+      feedback.pdf = feedback.pdf.split("~~")[0];
+    }
     res.send(feedback);
   } catch {
     (err) => console.log(err);
@@ -36,55 +53,60 @@ router.post("/", authenticate("user"), async (req, res, next) => {
   }
 });
 
-router.post("/create", authenticate("user"), async (req, res, next) => {
-  // console.log(req.files);
-  let uploadFile;
-  if (req.files !== null) {
-    uploadFile = req.files.pdf;
-    const name = uploadFile.name.split(".")[0];
-    const extension = uploadFile.name.split(".")[1];
-    const fileName = (name + "~~" + Date.now() + "." + extension).toString();
+router.post(
+  "/create",
+  authenticate("user"),
+  csrfProtection,
+  async (req, res, next) => {
+    // console.log(req.files);
+    let uploadFile;
+    if (req.files !== null) {
+      uploadFile = req.files.pdf;
+      const name = uploadFile.name.split(".")[0];
+      const extension = uploadFile.name.split(".")[1];
+      const fileName = (name + "~~" + Date.now() + "." + extension).toString();
 
-    if (fileName.includes("/") || fileName.includes("\\")) {
-      res.status(400).send("Error with filename");
-    } else {
-      uploadFile.mv(`${__dirname}/../pdfs/${fileName}`, async function (err) {
-        if (err) {
-          return res.status(500).send(err);
-        } else {
-          const createdUser = await db.Feedback.create({
-            // username: req.body.username,
-            email: req.body.email,
-            name: req.body.name,
-            comment: req.body.comment,
-            pdf: fileName,
-          });
-          const status = await createdUser.save();
-          if (status) {
-            res.send("WooHoo");
+      if (fileName.includes("/") || fileName.includes("\\")) {
+        res.status(400).send("Error with filename");
+      } else {
+        uploadFile.mv(`${__dirname}/../pdfs/${fileName}`, async function (err) {
+          if (err) {
+            return res.status(500).send(err);
+          } else {
+            const createdUser = await db.Feedback.create({
+              // username: req.body.username,
+              email: req.body.email,
+              name: req.body.name,
+              comment: req.body.comment,
+              pdf: fileName,
+            });
+            const status = await createdUser.save();
+            if (status) {
+              res.send("WooHoo");
+            }
           }
-        }
+        });
+      }
+    } else {
+      const createdUser = await db.Feedback.create({
+        // username: req.body.username,
+        email: req.user.email,
+        name: req.user.name,
+        comment: req.body.comment,
       });
+      const status = await createdUser.save();
+      if (status) {
+        res.send("WooHoo");
+      }
     }
-  } else {
-    const createdUser = await db.Feedback.create({
-      // username: req.body.username,
-      email: req.user.email,
-      name: req.user.name,
-      comment: req.body.comment,
-    });
-    const status = await createdUser.save();
-    if (status) {
-      res.send("WooHoo");
-    }
+    // console.log(uploadFile);
+
+    // console.log(fileName);
+
+    // console.log(fileName);
+    // res.send("success");
   }
-  // console.log(uploadFile);
-
-  // console.log(fileName);
-
-  // console.log(fileName);
-  // res.send("success");
-});
+);
 
 router.put("/update", async (req, res, next) => {
   let uploadFile;
@@ -92,7 +114,7 @@ router.put("/update", async (req, res, next) => {
     uploadFile = req.files.pdf;
     const name = uploadFile.name.split(".")[0];
     const extension = uploadFile.name.split(".")[1];
-    const fileName = (name + Date.now() + "." + extension).toString();
+    const fileName = (name + "~~" + Date.now() + "." + extension).toString();
 
     if (fileName.includes("/") || fileName.includes("\\")) {
       res.status(400).send("Error with filename");
@@ -144,7 +166,7 @@ router.put("/update", async (req, res, next) => {
   }
 });
 
-router.delete("/delete/:id", async (req, res, next) => {
+router.delete("/:id", async (req, res, next) => {
   // console.log(req.body);
   const deletableFeedback = await db.Feedback.findOne({
     where: { id: req.params.id },
@@ -172,6 +194,37 @@ router.delete("/delete/:id", async (req, res, next) => {
 
   // await deletableFeedback.save();
 });
+
+router.get(
+  "/file/:feedbackId",
+  authenticate("admin"),
+  async (req, res, next) => {
+    const { feedbackId } = req.params;
+    const feedback = await db.Feedback.findOne({
+      where: { id: feedbackId },
+    });
+
+    if (feedback === null) {
+      res.status(404).send("No such feedback");
+    }
+    console.log(feedback);
+    var fileToSend = `${__dirname}/../pdfs/${feedback.pdf}`;
+    const file = fileSystem.readFile(fileToSend, (err, myFile) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res
+          .setHeader("Content-Type", "application/pdf")
+          .setHeader(
+            "Content-Disposition",
+            `attachment; filename=${feedback.pdf.split("~~")[0]}.pdf`
+          )
+          .status(201)
+          .send(myFile);
+      }
+    });
+  }
+);
 
 module.exports = router;
 ``;
